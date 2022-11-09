f8b9cc931bd4df2471ff66580474e3aba001c2dc
Chesney-Bal <chesneybal@gmail.com>
2022-11-09 11:26:45 -0800

    Arithmetic functions completed

--- arithmetic.py
+++ arithmetic.py
@@ -3,33 +3,46 @@
 
 def add(num1, num2):
     """Return the sum of num1 and num2."""
-
-    return 10
+    sum_num= (num1 + num2)
+    return sum_num
 
 
 def subtract(num1, num2):
     """Return the value of num1 minus num2."""
-
+    diff_num= (num1 - num2)
+    return diff_num
 
 def multiply(num1, num2):
     """Multiply the num1 by num2 and return the result."""
+    product= (num1 * num2)
+    return product
 
 
 def divide(num1, num2):
     """Divide the num1 by num2, returning a floating point."""
+    quotient= (num1 / num2)
+    return quotient
 
 
 def square(num1):
     """Return the square of num1."""
+    sqrd= (num1 ** 2)
+    return sqrd
 
 
 def cube(num1):
     """Return the cube of num1."""
+    cubed= (num1 **3)
+    return (cubed)
 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
+    powered=(num1 ** num2)
+    return (powered)
 
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    modulus=(num1 % num2)
+    return modulus
